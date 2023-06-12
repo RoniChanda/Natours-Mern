d21f895260b439fd2b278dd3424499d2a0718cf2
Supratim <suprachanda97@gmail.com>
2023-06-13 00:33:22 +0530

    Change in webhook

--- backend/controllers/bookingController.js
+++ backend/controllers/bookingController.js
@@ -10,7 +10,7 @@ const User = require("../models/userModel");
 const createBookingCheckout = async (session) => {
   const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email })).id;
-  const price = session.display_items[0].price_data.unit_amount / 100;
+  const price = session.amount_total / 100;
 
   await Booking.create({ tour, user, price });
 };
@@ -65,7 +65,7 @@ exports.webhookCheckout = catchAsync(async (req, res, next) => {
   }
 
   if (event.type === "checkout.session.completed") {
-    await createBookingCheckout(event.data.object);
+    createBookingCheckout(event.data.object);
   }
 
   res.status(200).json({ received: true });
